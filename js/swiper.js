cb65efadfc540874cf0e8adcf8642f19a58d805e
aldm <d44.a1@yandex.ru>
2024-10-27 20:10:49 +0300

    1

--- js/swiper.js
+++ js/swiper.js
@@ -19,3 +19,25 @@ const swiper = new Swiper(".catalog__swiper", {
     el: ".swiper-scrollbar",
   },
 });
+
+const swiperInner = new Swiper(".mob-swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
